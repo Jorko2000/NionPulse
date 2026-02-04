b13b660392ffb65490a7def56a815501fd29a7bb
Jorko2000 <george1102@abv.bg>
2026-02-04 18:03:11 +0200

    test_events.py

--- services/api/tests/test_events.py
+++ services/api/tests/test_events.py
@@ -6,12 +6,3 @@ client = TestClient(app)
 def test_create_event():
     r = client.post("/events/", json={"type": "test", "payload": {"a":1}})
     assert r.status_code == 200
-
-test_cache.py
-
-from services.api.services.cache import cache_result, get_cache
-
-def test_cache():
-    cache_result("key1", {"val": 123}, ttl=1)
-    val = get_cache("key1")
-    assert val == {"val": 123}
